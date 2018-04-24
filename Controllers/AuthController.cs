ceb1d2238560bf055e4364854ad04cc8ae0c4960
Nikolas Trabue <ntrabue@preferredphm.com>
2018-04-24 19:44:19 -0400

    Added registration for users

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,22 +16,24 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
         {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                ModelState.AddModelError("Username", "Username already exists.");
+
+
             // validate request
-
-            username = username.ToLower();
-
-            if (await _repo.UserExists(username))
-                return BadRequest("Username is already taken.");
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
             var userToCreate = new User
             {
-                Username = username
-
+                Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
